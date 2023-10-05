84d4caab0a34e28304acc552891c480a4570ca37
Olivier Perrin <olivier.perrin@loria.fr>
2023-10-05 11:31:18 +0200

    [Update]: Add GET one + POST

--- src/main/java/org/miage/intervenantservice/boundary/IntervenantRepresentation.java
+++ src/main/java/org/miage/intervenantservice/boundary/IntervenantRepresentation.java
@@ -1,11 +1,22 @@
 package org.miage.intervenantservice.boundary;
 
+import java.net.URI;
+import java.util.Optional;
+import java.util.UUID;
+import org.miage.intervenantservice.entity.Intervenant;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.transaction.Transactional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Controller
 @ResponseBody
@@ -23,4 +34,26 @@ public class IntervenantRepresentation {
         return ResponseEntity.ok(ir.findAll());
     }
     
+    // GET one
+    @GetMapping(value="/{intervenantId}")
+    public ResponseEntity<?> getIntervenant(@PathVariable("intervenantId") String id) {
+        return Optional.of(ir.findById(id))
+            .filter(Optional::isPresent)
+            .map(i -> ResponseEntity.ok(i.get()))
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<?> postIntervenant(@RequestBody Intervenant intervenant) {
+        Intervenant toSave = new Intervenant(UUID.randomUUID().toString(), 
+        intervenant.getNom(), 
+        intervenant.getPrenom(), 
+        intervenant.getCommune(), 
+        intervenant.getCodepostal());
+        Intervenant saved = ir.save(toSave);
+        URI uri = linkTo(IntervenantRepresentation.class).slash(saved.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
