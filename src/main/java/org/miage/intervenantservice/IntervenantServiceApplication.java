f0955fd202f32c9962905dc45b5530a4c59c1d85
Olivier Perrin <olivier.perrin@loria.fr>
2024-01-29 10:23:28 +0100

    Ajout de OpenAPI

--- src/main/java/org/miage/intervenantservice/IntervenantServiceApplication.java
+++ src/main/java/org/miage/intervenantservice/IntervenantServiceApplication.java
@@ -2,6 +2,9 @@ package org.miage.intervenantservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
 public class IntervenantServiceApplication {
@@ -10,4 +13,11 @@ public class IntervenantServiceApplication {
 		SpringApplication.run(IntervenantServiceApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI intervenantAPI() {
+		return new OpenAPI().info(new Info()
+			.title("Intervenant API")
+			.version("1.0")
+			.description("Documentation Intervenant API Miage"));
+	}
 }
